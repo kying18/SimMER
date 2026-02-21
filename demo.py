f58fff6833b6de5a3de6ef67f99a24c92c62157b
Kylie Ying <22729429+kying18@users.noreply.github.com>
2026-02-21 14:51:01 -0800

    Add ability to query joint positions.

--- demo.py
+++ demo.py
@@ -1,16 +1,20 @@
 # Forward Kinematics Demo
 
 import simmer
+import numpy as np
 
 # Define the joints and links
 joint1 = simmer.Joint(angle=45.0, radians=False, name="shoulder")
-link1 = simmer.Link(length=2.0, name="upper arm")
+link1 = simmer.Link(length=np.sqrt(8), name="upper arm")
 joint2 = simmer.Joint(angle=-15.0, radians=False, name="elbow")
-link2 = simmer.Link(length=3.0, name="lower arm")
+link2 = simmer.Link(length=2.0, name="lower arm")
 
 # Define the chain
-chain = simmer.Chain(origin=[0.0, 0.0], links=[link1, link2], joints=[joint1, joint2])
+chain = simmer.Chain(links=[link1, link2], joints=[joint1, joint2])
 
 # Calculate the forward kinematics
-end_effector = chain.forward_kinematics()
-print(f"End effector position: {end_effector[:2]}")
+chain.forward_kinematics()
+end_effector = chain.get_end_effector_position()
+joint_positions = chain.get_joint_positions()
+print(f"End effector position: {end_effector}")
+print(f"Joint positions: {joint_positions}")
--- simmer.py
+++ simmer.py
@@ -1,10 +1,12 @@
 from typing import Iterable
 import numpy as np
 
+ORIGIN = np.array([0.0, 0.0, 1.0])
+
 class Joint:
   def __init__(self, angle: float = 0.0, radians: bool = True, name: str = None):
-    # The angle of the joint defined as the angle between the link and the previous link
-    # If radians is False, the angle is in degrees
+    # The angle of the joint, in radians, defined as the angle 
+    # between the link and the previous link
     self.angle = angle if radians else angle * np.pi/180.0
     self.name = name
 
@@ -20,27 +22,38 @@ class Transform:
     self.joint = joint
     self.link = link
     self.transform = np.array(
-      [[np.cos(joint.angle), -np.sin(joint.angle), link.length * np.cos(joint.angle)],
-      [np.sin(joint.angle), np.cos(joint.angle), link.length * np.sin(joint.angle)],
+      [[np.cos(self.joint.angle), -np.sin(self.joint.angle), self.link.length * np.cos(self.joint.angle)],
+      [np.sin(self.joint.angle), np.cos(self.joint.angle), self.link.length * np.sin(self.joint.angle)],
       [0.0, 0.0, 1.0]])
 
 class Chain:
-  def __init__(self, origin: Iterable[float] = [0.0, 0.0], links: Iterable[Link] = [], joints: Iterable[Joint] = []):
+  def __init__(self,  links: Iterable[Link], joints: Iterable[Joint]):
     # The links of the chain and the joints that connect them
     # The links are connected to the joints in order and vice versa
     # The position of the first joint is defined by the origin 
     # and the position of the last link is the end effector
     self.links = links
     self.joints = joints
-    # Represent the origin as a homogeneous coordinate
-    self.origin_h = np.array(origin + [1.0])
+
+    assert len(self.links) == len(self.joints), "The number of links and joints must be the same"
+    assert len(self.links) > 0, "The chain must have at least one link"
 
   def forward_kinematics(self):
     # Calculate the forward kinematics of the chain
     # Returns the position of the end effector in homogeneous coordinates
     self.transforms = [Transform(joint, link) for joint, link in zip(self.joints, self.links)]
-    # Initialize the solution as the identity matrix
-    solution = np.identity(3)
-    for transform in self.transforms:
-      solution = solution @ transform.transform
-    return solution @ self.origin_h
+    
+    solution = self.transforms[0].transform
+    self.joint_positions = np.zeros((len(self.joints) + 1, 2))
+
+    for i in range(1, len(self.transforms)):
+      self.joint_positions[i] = (solution @ ORIGIN)[:2]
+      solution = solution @ self.transforms[i].transform
+
+    self.joint_positions[len(self.joints)] = (solution @ ORIGIN)[:2]
+
+  def get_end_effector_position(self):
+    return self.joint_positions[-1]
+
+  def get_joint_positions(self):
+    return self.joint_positions[:-1]
